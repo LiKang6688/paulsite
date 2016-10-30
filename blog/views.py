d3cd076d56e113840a7daab57d186ae54a2701fa
Kangli <kkll7766@icloud.com>
2016-10-30 20:02:53 +0100

    first commit

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
-# from django.shortcuts import render
+# from django.shortcuts import get_object_or_404,render
 
 from .models import Question
 
@@ -22,16 +22,21 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
     # context = {'latest_question_list': latest_question_list}
-    # return render(request, 'polls/index.html', context)
+    # return render(request, 'blog/index.html', context)
+
 
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'blog/detail.html', {'question': question})
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
 
-def detail(request, question_id):
+# def detail(request, question_id):
+    # def detail(request, question_id):
+
     return HttpResponse("You're looking at question %s." % question_id)
 
 
